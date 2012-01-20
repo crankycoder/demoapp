092165c9488e8ea631b84fd02a8e1e737626d759
Victor Ng <crankycoder@gmail.com>
2012-01-20 15:26:43 -0800

    added automatic timers and counts on controller methods

--- demoapp/views.py
+++ demoapp/views.py
@@ -4,24 +4,19 @@ from pyramid.exceptions import Forbidden
 from pyramid.security import authenticated_userid, effective_principals
 from pyramid.view import view_config
 
-from cornice import Service
-
 from mozsvc.metrics import timeit
-
-
+from mozsvc.metrics import MetricsService
 
 info_desc = """\
 This service is useful to get and set data for a user.
 """
 
-
-user_info = Service(name='users', path='/{username}/info',
+user_info = MetricsService(name='users', path='/{username}/info',
                     description=info_desc)
 
 _USERS = defaultdict(dict)
 
 @user_info.get()
-@timeit
 def get_info(request):
     """Returns the public information about a **user**.
 
@@ -46,7 +41,6 @@ def set_info(request):
     return {'success': True}
 
 
-#@timeit
 @view_config(route_name="whoami", permission="authenticated", renderer="json")
 def whoami(request):
     """View returning the authenticated user's credentials."""
